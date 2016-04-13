77f2b50d76fcf14c6a500912237e55a45588038b
Bartosz Chojnacki <bchojnacki@team.mobile.de>
2016-04-13 23:59:19 +0200

    style(style): style

--- index.test.js
+++ index.test.js
@@ -4,11 +4,9 @@ require("./index.js");
 
 describe('angular-global-resolve', function () {
   beforeEach(angular.mock.module('angular-global-resolve'));
-
   describe('when it works', function () {
     it('should work!', function () {
       expect(true).toBe(true);
     });
   });
-
 });
